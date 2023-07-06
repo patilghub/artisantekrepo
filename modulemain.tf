28c1b7058f52213102cdcb77e2cffe4e389ded92
patilghub <133686765+patilghub@users.noreply.github.com>
2023-07-06 12:29:39 +0530

    Update modulemain.tf

--- modulemain.tf
+++ modulemain.tf
@@ -1,13 +1,1 @@
-terraform {
-  required_version = ">=0.12"
-}
 
-
-
-resource "aws_instance" "ec2_module_1" {
-
-    ami = "ami-038056f5d3df2259d"
-    instance_type = "t2.micro"
-    key_name= "as"
-    
-}
